9913ffa0ff7d2e05e62d6110d5e9f81d8e98d332
sascha <sascha_burku@yahoo.de>
2015-10-02 19:02:14 +0200

    Set Input_Sanitizer styling

--- lib/money/input_sanitizer.rb
+++ lib/money/input_sanitizer.rb
@@ -1,22 +1,22 @@
 module Money
-	class InputSanitizer
-		############################################################################
-		# Method checks if the parameter is a numeric value and also if the numeric
-		# is positiv.
-		# params:
-		# +amount+:: A numeric value defining the amount of Money::LiveConverter instance.
-		# returns:
-		# The absolute numeric value. 
-		############################################################################
-		def self.sanitize_amount(amount)
-		  raise "Please use a positive number as first parameter. E.g. Money::LiveConverter.new(4,'EUR')" unless amount.is_a?(Numeric)
-			if amount < 0
-			   amount = amount*(-1)
-			   puts "Your amount was a negative number. We assume you meant a positive, so we use this value instead."
-			end
-			'%.2f'% amount
-		end
-		############################################################################
+  class InputSanitizer
+    ############################################################################
+    # Method checks if the parameter is a numeric value and also if the numeric
+    # is positiv.
+    # params:
+    # +amount+:: A numeric value defining the amount of Money::LiveConverter instance.
+    # returns:
+    # The absolute numeric value. 
+    ############################################################################
+    def self.sanitize_amount(amount)
+      raise "Please use a positive number as first parameter. E.g. Money::LiveConverter.new(4,'EUR')" unless amount.is_a?(Numeric)
+      if amount < 0
+         amount = amount*(-1)
+         puts "Your amount was a negative number. We assume you meant a positive, so we use this value instead."
+      end
+      '%.2f'% amount
+    end
+    ############################################################################
     # Method checks if the parameter is a String and if defined if the parameter
     # is within a set of values.
     # is positiv.
@@ -26,24 +26,24 @@ module Money
     # returns:
     # The String currency value. 
     ############################################################################
-		def self.sanitize_currency(currency, allowed_cross_rates=[])
-			raise "Argument Currency must be a String. E.g. Money::LiveConverter.new(4,'EUR')." unless currency.is_a?(String)
-			unless allowed_cross_rates.empty?
-			  raise "Argument Currency must be one of the following currencies: #{allowed_cross_rates}." unless allowed_cross_rates.include?(currency)
-			end
-			currency
-		end
-		############################################################################
+    def self.sanitize_currency(currency, allowed_cross_rates=[])
+      raise "Argument Currency must be a String. E.g. Money::LiveConverter.new(4,'EUR')." unless currency.is_a?(String)
+      unless allowed_cross_rates.empty?
+        raise "Argument Currency must be one of the following currencies: #{allowed_cross_rates}." unless allowed_cross_rates.include?(currency)
+      end
+      currency
+    end
+    ############################################################################
     # Method checks if the parameter is a Hash value.
     # params:
     # +cross_rate+:: A Hash defining all available cross values for a given currency.
     # returns:
     # A Hash defining all available cross values 
     ############################################################################
-		def self.sanitize_cross_rate(cross_rate)
-		  raise "Please use a Hash in the form of {'USD' => 1.11,'Bitcoin' => 0.0046}." unless cross_rate.is_a?(Hash)
-		  cross_rate
-		end	
+    def self.sanitize_cross_rate(cross_rate)
+      raise "Please use a Hash in the form of {'USD' => 1.11,'Bitcoin' => 0.0046}." unless cross_rate.is_a?(Hash)
+      cross_rate
+    end  
 
     ############################################################################
     # Method checks if the parameter is a Money::LiveConverter instance.
@@ -57,5 +57,5 @@ module Money
       money_live_converter
     end
 
-	end
+  end
 end
