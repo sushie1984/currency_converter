cc95c969dbee2c8acb0fd2bd081d7b9358fe8593
sascha <sascha_burku@yahoo.de>
2015-10-02 19:08:11 +0200

    Set text styling of cross_rate.rb and money.rb

--- lib/money.rb
+++ lib/money.rb
@@ -3,38 +3,38 @@ require "money/input_sanitizer"
 require "money/cross_rate"
 
 module Money
-	class LiveConverter
-		attr_accessor :amount
-		attr_reader :currency
-		
-		#####################################################
-		# Basic Constructor:
-		# Call with Money::LiveConverter.new(amount,currency) 
-		# params:
- 		# +amount+:: Is a positive number reprasenting the money amount of this object
-		# +currency+:: Is a 3 char length based string (ISO 4217) representing 
-		# 				this object is holding 
-		# +options+:: An option hash for future purpose.
-		####################################################
-		def initialize(amount=1,currency='EUR',opt={})
-		  Money::CrossRate.conversion_rates if Money::CrossRate.currency.nil? && Money::CrossRate.cross_rate.nil?
-			begin
-				@amount = Money::InputSanitizer.sanitize_amount(amount)
-				@currency = Money::InputSanitizer.sanitize_currency(currency,Money::CrossRate.get_all_available_currencies)		
-			rescue RuntimeError => e
-				puts "#{e.message}"
-				@amount = nil
-				@currency = nil
-			end
-		end
-		
-		####################################################
-		# Overriding original inspect and returning a String representation of the
-		# Money::LiveConverter instance
-		####################################################
-		def inspect
-			"#{@amount} #{@currency}" if @amount && @currency
-		end
+  class LiveConverter
+    attr_accessor :amount
+    attr_reader :currency
+    
+    #####################################################
+    # Basic Constructor:
+    # Call with Money::LiveConverter.new(amount,currency) 
+    # params:
+     # +amount+:: Is a positive number reprasenting the money amount of this object
+    # +currency+:: Is a 3 char length based string (ISO 4217) representing 
+    #         this object is holding 
+    # +options+:: An option hash for future purpose.
+    ####################################################
+    def initialize(amount=1,currency='EUR',opt={})
+      Money::CrossRate.conversion_rates if Money::CrossRate.currency.nil? && Money::CrossRate.cross_rate.nil?
+      begin
+        @amount = Money::InputSanitizer.sanitize_amount(amount)
+        @currency = Money::InputSanitizer.sanitize_currency(currency,Money::CrossRate.get_all_available_currencies)    
+      rescue RuntimeError => e
+        puts "#{e.message}"
+        @amount = nil
+        @currency = nil
+      end
+    end
+    
+    ####################################################
+    # Overriding original inspect and returning a String representation of the
+    # Money::LiveConverter instance
+    ####################################################
+    def inspect
+      "#{@amount} #{@currency}" if @amount && @currency
+    end
     ###############################################################
     # Method sets the individuel cross_rate for a specific base currency value
     # params:
@@ -42,43 +42,43 @@ module Money
     # +cross_rate+:: A Hash in the manner of {'Currency' => Numeric_Value} 
     #                reprasenting the cross rate to the base currency value
     ###############################################################
-		def self.conversion_rates(currency,cross_rate)
-	     Money::CrossRate.conversion_rates(currency,cross_rate)
-	  end
-	  
-	  ##################################################################
-	  # Method converts the given amount of a Money::LiveConverter instance
-	  # into a new Money::LiveConverter instance based on the exchange_currency 
-	  # paramater. The new instance is returned if no errors happens during 
-	  # the convertation.
-	  # params:
-	  # +exchange_currency+:: A String defining what currency instance
-	  #                      is returned.
-	  # returns:
-	  # +Money::LiveConverter+: A new instance with the proper amount and
-	  #                         currency tag. 
-	  ##################################################################
-	  def convert_to(exchange_currency)
-	    begin
-	       exchange_currency = Money::InputSanitizer.sanitize_currency(exchange_currency,Money::CrossRate.get_all_available_currencies)
-	       Money::LiveConverter.new(@amount.to_f*Money::CrossRate.get_cross_value_for_currency(@currency,exchange_currency),exchange_currency)
-	    rescue RuntimeError => e
-	       puts "#{e.message}"
-	    end
-	  end
-	  ###################################################################
-	  # Overriding the plus operator by adding the amounts of each instance.
-	  # If the amount of the second instance is in a different currency
-	  # then the second instance call its convert_to function with the
-	  # currency of the first instance.
-	  # params:
-	  # +money_live_convertor+:: An instance of Money::LiveConvertor which
-	  #                          is going to be added to the first one.
-	  ###################################################################
-	  def +(money_live_convertor)
-	    arithmetic_op('+',self,money_live_convertor)    
-	  end
-	  ###################################################################
+    def self.conversion_rates(currency,cross_rate)
+       Money::CrossRate.conversion_rates(currency,cross_rate)
+    end
+    
+    ##################################################################
+    # Method converts the given amount of a Money::LiveConverter instance
+    # into a new Money::LiveConverter instance based on the exchange_currency 
+    # paramater. The new instance is returned if no errors happens during 
+    # the convertation.
+    # params:
+    # +exchange_currency+:: A String defining what currency instance
+    #                      is returned.
+    # returns:
+    # +Money::LiveConverter+: A new instance with the proper amount and
+    #                         currency tag. 
+    ##################################################################
+    def convert_to(exchange_currency)
+      begin
+         exchange_currency = Money::InputSanitizer.sanitize_currency(exchange_currency,Money::CrossRate.get_all_available_currencies)
+         Money::LiveConverter.new(@amount.to_f*Money::CrossRate.get_cross_value_for_currency(@currency,exchange_currency),exchange_currency)
+      rescue RuntimeError => e
+         puts "#{e.message}"
+      end
+    end
+    ###################################################################
+    # Overriding the plus operator by adding the amounts of each instance.
+    # If the amount of the second instance is in a different currency
+    # then the second instance call its convert_to function with the
+    # currency of the first instance.
+    # params:
+    # +money_live_convertor+:: An instance of Money::LiveConvertor which
+    #                          is going to be added to the first one.
+    ###################################################################
+    def +(money_live_convertor)
+      arithmetic_op('+',self,money_live_convertor)    
+    end
+    ###################################################################
     # Overriding the minus operator by subtracting the amounts of each instance.
     # If the amount of the second instance is in a different currency
     # then the second instance call its convert_to function with the
@@ -87,20 +87,20 @@ module Money
     # +money_live_convertor+:: An instance of Money::LiveConvertor which
     #                          is going to be subtracted to the first one.
     ###################################################################
-	  def -(money_live_convertor)  
-	    arithmetic_op('-',self,money_live_convertor)
-	  end
-	  ###################################################################
+    def -(money_live_convertor)  
+      arithmetic_op('-',self,money_live_convertor)
+    end
+    ###################################################################
     # Overriding the multiplication operator by multiplying the amount
     # The amount of the second instance has to be a numeric
     # params:
     # +value+:: A numeric value multiplicated with the first instance amount
     ###################################################################
-	  def *(value)
-	    arithmetic_op('*',self,value,false)
-	  end
-	  
-	  ####################################################################
+    def *(value)
+      arithmetic_op('*',self,value,false)
+    end
+    
+    ####################################################################
     # Overriding the division operator by dividing the amount
     # The amount of the second instance has to be a numeric and not null
     # params:
@@ -109,24 +109,24 @@ module Money
     def /(value)
       arithmetic_op('/',self,value,false) unless value == 0
     end
-	   
-	  #####################################################################
+     
+    #####################################################################
     # Overriding the equality operator by comparing the instances amount
     # params:
     # +money_live_convertor+:: An instance of Money::LiveConvertor which
     #                          is going to be compared to the first one.
     ###################################################################
-	  def ==(money_live_convertor)  
+    def ==(money_live_convertor)  
       arithmetic_op('==',self,money_live_convertor)
     end
-	  
-	  #####################################################################
+    
+    #####################################################################
     # Overriding the less operator by comparing the instances amount
     # params:
     # +money_live_convertor+:: An instance of Money::LiveConvertor which
     #                          is going to be compared to the first one.
     ###################################################################
-	  def <(money_live_convertor)  
+    def <(money_live_convertor)  
       arithmetic_op('<',self,money_live_convertor)
     end
     
@@ -139,23 +139,23 @@ module Money
     def >(money_live_convertor)  
       arithmetic_op('>',self,money_live_convertor)
     end
-	
-	  private
-	  #####################################################################
-	  # Arithmethic fuction that does all the magic for the +,-,*,/,==,<,>
-	  # operations. Based on http://stackoverflow.com/questions/13060239/send-and-math-operators
-	  # the ruby public_send method is used to implement and interpret
-	  # each operation correctly.
-	  # params:
-	  # +op+:: A String represanting one of +,-,*,/,==,<,> operations
-	  # +ins_1+:: An Object which is used on the left hand side of the +op+ parameter
-	  # +ins_2+:: An Object which is used on the right hand side of the +op+ parameter
-	  # +do_sanitizing+:: A boolean flag defining whether a Money::LiveConverter instance
-	  #                   has to be expected at +ins_2+ or not.
-	  # returns:
-	  # True for ==,<,> operations and a new Money::LiveConverter instance for +,-,*,/
+  
+    private
     #####################################################################
-	  def arithmetic_op(op,ins_1,ins_2,do_sanitizing=true)
+    # Arithmethic fuction that does all the magic for the +,-,*,/,==,<,>
+    # operations. Based on http://stackoverflow.com/questions/13060239/send-and-math-operators
+    # the ruby public_send method is used to implement and interpret
+    # each operation correctly.
+    # params:
+    # +op+:: A String represanting one of +,-,*,/,==,<,> operations
+    # +ins_1+:: An Object which is used on the left hand side of the +op+ parameter
+    # +ins_2+:: An Object which is used on the right hand side of the +op+ parameter
+    # +do_sanitizing+:: A boolean flag defining whether a Money::LiveConverter instance
+    #                   has to be expected at +ins_2+ or not.
+    # returns:
+    # True for ==,<,> operations and a new Money::LiveConverter instance for +,-,*,/
+    #####################################################################
+    def arithmetic_op(op,ins_1,ins_2,do_sanitizing=true)
       begin
         amount = ''
         if do_sanitizing
@@ -177,5 +177,6 @@ module Money
          puts "#{e.message}"
       end
     end
-	end  
+    
+  end  
 end
--- lib/money/cross_rate.rb
+++ lib/money/cross_rate.rb
@@ -1,54 +1,55 @@
 module Money
   class CrossRate
+    ############################################################################
+    # HINT/Lessons learnt:
+    # As to http://stackoverflow.com/questions/895747/how-can-rubys-attr-accessor-produce-class-variables-or-class-instance-variables
+    # this is similar to rails cattr_accessor
+    ############################################################################
+    class << self
+      attr_accessor :currency
+      attr_accessor :cross_rate
       ############################################################################
-      # HINT/Lessons learnt:
-      # As to http://stackoverflow.com/questions/895747/how-can-rubys-attr-accessor-produce-class-variables-or-class-instance-variables
-      # this is similar to rails cattr_accessor
+      # Setting the conversion rates
+      # according to a base currency.
+      # params:
+      # +currency+:: Is a String defining the base currency
+      # +cross_rate+:: Is a Hash containing all available rates
+      #                in correlation with the currency.
       ############################################################################
-      class << self
-        attr_accessor :currency
-        attr_accessor :cross_rate
-        ############################################################################
-        # Setting the conversion rates
-        # according to a base currency.
-        # params:
-        # +currency+:: Is a String defining the base currency
-        # +cross_rate+:: Is a Hash containing all available rates
-        #                in correlation with the currency.
-        ############################################################################
-        def conversion_rates(currency="EUR",cross_rate={'USD' => 1.11, 'Bitcoin' => 0.0047})
-          @currency = Money::InputSanitizer.sanitize_currency(currency)
-          @cross_rate = Money::InputSanitizer.sanitize_cross_rate(cross_rate)
-        end
-        ############################################################################
-        # Returns an aray containing all possible currencies.
-        # returns:
-        # +[Array]+: An array containg all available currencies.
-        ############################################################################
-        def get_all_available_currencies
-          [@currency].concat @cross_rate.keys
-        end
-        
-        ############################################################################
-        # Methods calculates a cross-value for two currencies. The first
-        # currencies therefore defines what cross-rate is the base one, wheras
-        # the second one defines in what currency it should be converted. 
-        # If the first one is not the one defined in @currency then depending
-        # on the second one a calculation of the corresponding cross-value is done.
-        # params:
-        # +base_currency+:: The base currency that is used for further converting ops.
-        # +exchange_currency+:: The currency that base_currency is going to be converted.
-        # returns:
-        # A Numeric Object containing the cross-value.
-        ############################################################################
-        def get_cross_value_for_currency(base_currency,exchange_currency)
-          return @cross_rate[exchange_currency] if base_currency == @currency
-          return (1.00/@cross_rate[base_currency] ) if (exchange_currency == @currency)
-          b_curr_to_exch_curr = get_cross_value_for_currency(@currency,base_currency)
-          exch_curr_to_b_curr = get_cross_value_for_currency(exchange_currency,@currency)
-          return b_curr_to_exch_curr*exch_curr_to_b_curr     
-        end  
-        
-      end    
+      def conversion_rates(currency="EUR",cross_rate={'USD' => 1.11, 'Bitcoin' => 0.0047})
+        @currency = Money::InputSanitizer.sanitize_currency(currency)
+        @cross_rate = Money::InputSanitizer.sanitize_cross_rate(cross_rate)
+      end
+
+      ############################################################################
+      # Returns an aray containing all possible currencies.
+      # returns:
+      # +[Array]+: An array containg all available currencies.
+      ############################################################################
+      def get_all_available_currencies
+        [@currency].concat @cross_rate.keys
+      end
+
+      ############################################################################
+      # Methods calculates a cross-value for two currencies. The first
+      # currencies therefore defines what cross-rate is the base one, wheras
+      # the second one defines in what currency it should be converted.
+      # If the first one is not the one defined in @currency then depending
+      # on the second one a calculation of the corresponding cross-value is done.
+      # params:
+      # +base_currency+:: The base currency that is used for further converting ops.
+      # +exchange_currency+:: The currency that base_currency is going to be converted.
+      # returns:
+      # A Numeric Object containing the cross-value.
+      ############################################################################
+      def get_cross_value_for_currency(base_currency,exchange_currency)
+        return @cross_rate[exchange_currency] if base_currency == @currency
+        return (1.00/@cross_rate[base_currency] ) if (exchange_currency == @currency)
+        b_curr_to_exch_curr = get_cross_value_for_currency(@currency,base_currency)
+        exch_curr_to_b_curr = get_cross_value_for_currency(exchange_currency,@currency)
+        return b_curr_to_exch_curr*exch_curr_to_b_curr
+      end
+
+    end
   end
 end
